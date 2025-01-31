6863003fa5808bf3fd82cd7c8365ef767e9a58c1
Weslley90 <weslleycarvalho344@gmail.com>
2025-01-30 22:58:17 -0300

    upload de codigo quebrado

--- src/script.js
+++ src/script.js
@@ -33,9 +33,9 @@ document.querySelector('button[type="submit"]')
     if (isPhoneRequired && !phoneField.value) {
       return showAndHideErrorMessage()
     }
-    if (!emailField.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
-      return showAndHideErrorMessage()
-    }
+    // if (!emailField.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+    //   return showAndHideErrorMessage()
+    // }
     firstNameField.value = ''
     lastNameField.value = ''
     emailField.value = ''
